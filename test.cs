c891ff643a6c5c5dce92c081a0377d96d68aa612
manavdholakiya2123 <manav.dholakiya@matrixcomsec.com>
2025-02-04 06:41:26 +0000

    testcommit3

---
+++ test.cs
@@ -0,0 +1,31 @@
+// basic calculator application
+
+using System;
+
+// add methods to the class
+
+class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+
+
